cc74efbe444fa287dacb5f517772bc99755ac329
antgarcia12 <antgarcia014@gmail.com>
2021-02-25 20:33:31 -0700

    adding files

---
+++ index.js
@@ -0,0 +1,5 @@
+// https://dog.ceo/dog-api/
+// - Use this API along with axios to get the url to a random image for an Otterhound
+
+// log the url to the console. make a Github respository to store this code
+
